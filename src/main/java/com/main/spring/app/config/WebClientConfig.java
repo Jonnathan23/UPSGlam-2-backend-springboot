4f3fdb6d0edeccbf777525a9f9fece2ef32d103d
Jonnathan23 <jsaquicela26@gmail.com>
2025-12-07 12:33:41 -0500

    Agregar nueva url al Config y creacion del templeta properties

--- src/main/java/com/main/spring/app/config/WebClientConfig.java
+++ src/main/java/com/main/spring/app/config/WebClientConfig.java
@@ -11,12 +11,26 @@ public class WebClientConfig {
     @Value("${fastapi.url}")
     private String fastApiUrl;
 
-    @Bean
-    public WebClient webClient(WebClient.Builder builder) {
+    @Value("${firebase.api.key}")
+    private String firebaseApiKey;
+
+    private static final String FIREBASE_AUTH_BASE_URL = "https://identitytoolkit.googleapis.com/v1";
+
+    @Bean(name = "firebaseAuthWebClient")
+    public WebClient firebaseAuthWebClient(WebClient.Builder builder) {
+
         return builder
-                .baseUrl(fastApiUrl)
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10MB buffer for
-                                                                                                    // images
+                .baseUrl(FIREBASE_AUTH_BASE_URL)
+                .build();
+    }
+
+    // 2. WebClient para el Servicio de FastAPI
+    @Bean(name = "fastApiWebClient")
+    public WebClient fastApiWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl(fastApiUrl) // URL base: localhost:8000
+                // Configuramos el buffer grande para la subida o descarga de imágenes
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024))
                 .build();
     }
 }
